07e7f564d871c3e39a966feb05a0e16722328267
mariorodon <mariorodon2022@gmail.com>
2025-10-25 15:33:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -187,28 +187,6 @@ atualizarCarrinho = function() {
 
 atualizarContadorCarrinho();
 
-// ==== BOTÃO FLUTUANTE DO CARRINHO ====
-function atualizarContadorCarrinho() {
-  const contador = document.getElementById("contadorCarrinho");
-  const botao = document.getElementById("botaoCarrinhoFlutuante");
-  if (!contador || !botao) return;
-
-  const totalItens = carrinho.reduce((sum, i) => sum + i.quantidade, 0);
-  contador.textContent = totalItens;
-  botao.style.display = totalItens > 0 ? "flex" : "none";
-}
-
-function rolarCarrinho() {
-  document.querySelector(".carrinho").scrollIntoView({ behavior: "smooth" });
-}
-
-const oldAtualizarCarrinho2 = atualizarCarrinho;
-atualizarCarrinho = function() {
-  oldAtualizarCarrinho2();
-  atualizarContadorCarrinho();
-};
-
-atualizarContadorCarrinho();
 
 
 atualizarCarrinho();
