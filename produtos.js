1d38dc6c59a25dfde91f7987da203b0ced0fa22e
mariorodon <mariorodon2022@gmail.com>
2026-04-10 15:28:33 -0300

    Create produtos.js

---
+++ produtos.js
@@ -0,0 +1,40 @@
+const produtos = [
+  {
+    categoria: "🍔 Lanches",
+    itens: [
+      { nome: "X-Salada", descricao: "Pão macio, carne de hambúrguer suculento, queijo, ovo, milho, ervilha, alface, tomate e maionese especial.", preco: 23 },
+      { nome: "X-Bacon", descricao: "Hambúrguer artesanal, queijo derretido, crocantes fatias de bacon e molho da casa.", preco: 26 },
+      { nome: "X-Burguer I", descricao: "Pão, Hambúrguer, Presunto, Queijo, Ovo, Maionese Caseira.", preco: 21 },
+      { nome: "X-Burguer II", descricao: "2 Pão, Hambúrguer em Dobro, Presunto, Queijo, Ovo, Maionese Caseira.", preco: 24 },
+      { nome: "X-Frango", descricao: "Pão, Frango, Presunto, Queijo, Ovo, Milho, Ervilha, Alface, Tomate, Maionese Caseira.", preco: 24 },
+      { nome: "X-Frango c/ Calabresa", descricao: "Pão, Frango, Calabresa, Presunto, Queijo, Ovo, Milho, Ervilha, Alface, Tomate, Maionese Caseira.", preco: 27 },
+      { nome: "X-Calabresa", descricao: "Pão, Hambúrguer, Presunto, Queijo, Ovo, Milho, Alface, Ervilha, Tomate, Maionese Caseira.", preco: 26 },
+      { nome: "X-Coração", descricao: "Pão, Hambúrguer, Coração, Presunto, Queijo, Ovo, Milho, Ervilha, Alface, Tomate, Maionese Caseira.", preco: 27 },
+      { nome: "X-Coração c/ Bacon", descricao: "Pão, Hambúrguer, Coração, Bacon, Presunto, Queijo, Ovo, Milho, Ervilha, Alface, Tomate, Maionese Caseira.", preco: 30 },
+      { nome: "X-Coração c/ Frango", descricao: "Pão, Hambúrguer, Coração, Frango, Queijo, Ovo, Milho, Ervilha, Alface, Tomate, Maionese Caseira.", preco: 30 },
+      { nome: "X-Acebolado", descricao: "Pão, Hambúrguer, Presunto, Queijo, Ovo, Cebola caramelizada, Maionese Caseira.", preco: 24 },
+      { nome: "X-Tudo", descricao: "Pão, Hambúrguer, Coração, Presunto, Bacon, Queijo, Ovo, Milho, Ervilha, Alface, Tomate, Maionese Caseira.", preco: 31 },
+      { nome: "X-Sem Pão", descricao: "Versão leve sem pão, com hambúrguer artesanal, queijo, salada fresca e molho da casa.", preco: 22 }
+    ]
+  },
+  {
+    categoria: "🥤 Bebidas",
+    itens: [
+      { nome: "Refrigerante 350ml", descricao: "Lata 350ml", preco: 7 },
+      { nome: "Água Mineral 500ml", descricao: "Garrafa 500ml", preco: 5 },
+      { nome: "Refrigerante 600ml", descricao: "Garrafa 600ml", preco: 10 },
+      {
+    nome: "Refrigerante 2L",
+    descricao: "Escolha o sabor disponível",
+    preco: 15,
+    tipo: "modalRefrigerante"
+    }
+    ]
+  },
+  {
+    categoria: "🍟 Porções",
+    itens: [
+      { nome: "Porção de Batata Frita 400g", descricao: "Porção generosa", preco: 25 }
+    ]
+  }
+];
